f01ac0787fe395f088f81f0b70c4fe403b40d7ef
ridrisr <79858083+RidRisR@users.noreply.github.com>
2026-03-15 21:45:05 +0800

    feat: add DHRuleData type definition

---
+++ plugins/daggerheart/types.ts
@@ -0,0 +1,17 @@
+// plugins/daggerheart/types.ts
+export interface DHRuleData {
+  agility: number
+  strength: number
+  finesse: number
+  instinct: number
+  presence: number
+  knowledge: number
+  tier: 1 | 2 | 3 | 4
+  proficiency: number
+  className: string
+  ancestry: string
+  hp: { current: number; max: number }
+  stress: { current: number; max: number }
+  hope: number
+  armor: number
+}
