b5d2af64a85c7198a3025ddcb1065f6b4795e8dc
ridrisr <79858083+RidRisR@users.noreply.github.com>
2026-03-17 01:12:03 +0800

    chore: ignore commitlint.config.ts in eslint

--- eslint.config.js
+++ eslint.config.js
@@ -7,7 +7,7 @@ import prettier from 'eslint-config-prettier'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
-  globalIgnores(['dist', '.worktrees', 'coverage']),
+  globalIgnores(['dist', '.worktrees', 'coverage', 'commitlint.config.ts']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
